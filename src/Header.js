bedac57ac6c5b9f54c7c3ad9ec0332dcda925aa8
Jackson <jacksonlango@gmail.com>
2017-05-06 00:29:07 -0700

    start react portfolio site with header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export function Header(props) {
+  return (
+    <div className="App-header">
+      <h2>Jackson Lango</h2>
+      <div>
+      <a href="./games">Games</a> 
+      <a href="./programming">Programming Resume</a>
+      <a href="#">Fine Art</a>
+      <a href="#">Twitter</a>
+      <a href="#">Github</a>
+      </div>
+    </div>
+  )
+}
