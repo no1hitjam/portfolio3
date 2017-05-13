7e613f02c42d1c1565acafebaa67814d93fe5b28
Jackson Lango <jacksonlango@gmail.com>
2017-05-12 17:20:54 -0700

    make header look better at super thin resolutions

--- src/Header.js
+++ src/Header.js
@@ -13,13 +13,15 @@ export function Header(props) {
   return (
     <div id="App-header-container">
       <div id="App-header">
-        <Link to="/"><h2><img alt="portfolio" src={homeImg} /> &nbsp; Jackson Lango &nbsp; <img alt="portfolio" src={home2Img} /></h2></Link>
+        <Link id="header-home" to="/"><span><img alt="" src={homeImg} /> Jackson Lango <img alt="" src={home2Img} /></span></Link>
         <div>
         <Link to="/games">Games</Link> 
         <Link to="./programming">Programming</Link>
+        <span style={{display: 'inline-block'}}>
         <a href="https://www.instagram.com/moon_dude/"><img src={instagramImg} /></a>
         <a href="https://twitter.com/no1hitjam"><img src={twitterImg} /></a>
         <a href="https://github.com/no1hitjam"><img src={githubImg} /></a>
+        </span>
         </div>
       </div>
     </div>
