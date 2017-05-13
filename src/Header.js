2d033d6c859f29a51b113a5a7dd7dbcfd8f60a7f
Jackson Lango <jacksonlango@gmail.com>
2017-05-12 17:05:26 -0700

    improve header

--- src/Header.js
+++ src/Header.js
@@ -11,14 +11,16 @@ import githubImg from './img/github.png';
 
 export function Header(props) {
   return (
-    <div className="App-header">
-      <Link to="/"><h2><img alt="portfolio" src={homeImg} /> &nbsp; Jackson Lango &nbsp; <img alt="portfolio" src={home2Img} /></h2></Link>
-      <div>
-      <Link to="/games">Games</Link> 
-      <Link to="./programming">Programming</Link>
-      <a href="https://www.instagram.com/moon_dude/"><img src={instagramImg} /></a>
-      <a href="https://twitter.com/no1hitjam"><img src={twitterImg} /></a>
-      <a href="https://github.com/no1hitjam"><img src={githubImg} /></a>
+    <div id="App-header-container">
+      <div id="App-header">
+        <Link to="/"><h2><img alt="portfolio" src={homeImg} /> &nbsp; Jackson Lango &nbsp; <img alt="portfolio" src={home2Img} /></h2></Link>
+        <div>
+        <Link to="/games">Games</Link> 
+        <Link to="./programming">Programming</Link>
+        <a href="https://www.instagram.com/moon_dude/"><img src={instagramImg} /></a>
+        <a href="https://twitter.com/no1hitjam"><img src={twitterImg} /></a>
+        <a href="https://github.com/no1hitjam"><img src={githubImg} /></a>
+        </div>
       </div>
     </div>
   )
