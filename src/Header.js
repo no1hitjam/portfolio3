85d711d021106b001a101f594239873ef3bb5231
Jackson <jacksonlango@gmail.com>
2017-05-13 19:33:58 -0700

    make home page thinner vertically

--- src/Header.js
+++ src/Header.js
@@ -13,16 +13,22 @@ export function Header(props) {
   return (
     <div id="App-header-container">
       <div id="App-header">
-        <Link id="header-home" to="/"><span><img alt="" src={homeImg} /> Jackson Lango <img alt="" src={home2Img} /></span></Link>
-        <div>
-        <Link to="/games">Games</Link> 
-        <Link to="./programming">Programming</Link>
+        <Link id="header-home" to="/">
+          <span style={{display: 'inline-block'}}>
+            <span id="header-home-inner">
+              <img alt="" src={homeImg} /> Jackson Lango
+            </span>
+          </span>
+        </Link>
         <span style={{display: 'inline-block'}}>
-        <a href="https://www.instagram.com/moon_dude/"><img src={instagramImg} /></a>
-        <a href="https://twitter.com/no1hitjam"><img src={twitterImg} /></a>
-        <a href="https://github.com/no1hitjam"><img src={githubImg} /></a>
+          <Link to="/games">Games</Link> 
+          <Link to="./programming">Programming</Link>
+          <span style={{display: 'inline-block'}}>
+            <a href="https://www.instagram.com/moon_dude/"><img src={instagramImg} /></a>
+            <a href="https://twitter.com/no1hitjam"><img src={twitterImg} /></a>
+            <a href="https://github.com/no1hitjam"><img src={githubImg} /></a>
+          </span>
         </span>
-        </div>
       </div>
     </div>
   )
